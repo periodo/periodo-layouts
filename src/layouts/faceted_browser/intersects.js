489610de3a88b09347baa21f3f3353774572f2d8
Patrick Golden <ptgolden0@gmail.com>
2016-08-24 19:16:20 -0400

    no var!

--- src/layouts/faceted_browser/intersects.js
+++ src/layouts/faceted_browser/intersects.js
@@ -2,8 +2,8 @@
 
 // Given a set of limits and two x-coordinates for a line segment,
 module.exports = function ([lowerLimit, upperLimit], [x1, x2], allWithin=false) {
-  var lowerWithin = lowerLimit <= x1 && x1 <= upperLimit
-    , upperWithin = lowerLimit <= x2 && x2 <= upperLimit
+  const lowerWithin = lowerLimit <= x1 && x1 <= upperLimit
+      , upperWithin = lowerLimit <= x2 && x2 <= upperLimit
 
   if (allWithin) {
     return lowerWithin && upperWithin;
